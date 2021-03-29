6ddaf159f8d694126ec24cbd675b9c191f716487
aobrador <alexobrador1619@gmail.com>
2021-03-29 16:37:44 -0400

    change color scheme on learn page and typos

--- src/components/LearnInformationRow.js
+++ src/components/LearnInformationRow.js
@@ -5,24 +5,24 @@ const LearnInformationRow = () => {
   return (
     <div className='row-container container-fluid home-row'>
       <div className='image-container'>
-        <div className='image transportaion' />
+        <div className='image transportation' />
         <div className='title'>Transportation</div>
         <div className='text'>
-          Learn about how you chose to travel.
+          Learn how your choice of travel can affect the environment.
         </div>
       </div>
       <div className='image-container'>
         <div className='image household' />
         <div className='title'>Household</div>
         <div className='text'>
-          Learn how to better yourself and lower your emissions.
+          Learn how much energy is being used and lower your emissions.
         </div>
       </div>
       <div className='image-container'>
         <div className='image food' />
         <div className='title'>Food</div>
         <div className='text'>
-          Learn about the foods you consume that can affect the environment.
+          Learn how the foods you consume can affect the environment. 
         </div>
       </div>
       </div>
